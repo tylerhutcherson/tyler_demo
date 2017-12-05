ad6957bb561a61a69549d260323836ee55ea2ab6
tylerhutcherson <tyler.hutcherson@metismachine.com>
2017-12-05 17:36:51 -0500

    turn this into a mini data demo

--- index.py
+++ index.py
@@ -1,5 +1,37 @@
-## tyler_demo
 
-import os
+# coding: utf-8
 
-print("Hello, world!")
+# In[1]:
+
+from skafossdk import *
+
+ska = Skafos()
+
+
+# In[4]:
+
+ska.engine.create_view(
+    "power_datum", 
+    {"keyspace": "power", "table": "hist_load"}, 
+    DataSourceType.Cassandra).result()
+
+
+# In[5]:
+
+power = ska.engine.query("SELECT * from power_datum limit 10").result()['data']
+
+
+# In[9]:
+
+power
+
+
+# In[12]:
+
+import pandas as pd
+
+
+# In[8]:
+
+pd.DataFrame.from_dict(power)
+
