7a3f77cce4fb44583b91cd5edb5b091a9558c4a5
tylerhutcherson <tyler.hutcherson@metismachine.com>
2017-12-05 17:45:45 -0500

    remove pandas for now

--- index.py
+++ index.py
@@ -1,37 +1,16 @@
-
-# coding: utf-8
-
-# In[1]:
-
+## Tyler Demo
 from skafossdk import *
 
 ska = Skafos()
-
-
-# In[4]:
-
 ska.engine.create_view(
     "power_datum", 
     {"keyspace": "power", "table": "hist_load"}, 
     DataSourceType.Cassandra).result()
 
 
-# In[5]:
+print('Query some data!!!')
+power = ska.engine.query("SELECT * from power_datum limit 10").result()
 
-power = ska.engine.query("SELECT * from power_datum limit 10").result()['data']
+print(power['data'])
 
-
-# In[9]:
-
-power
-
-
-# In[12]:
-
-import pandas as pd
-
-
-# In[8]:
-
-pd.DataFrame.from_dict(power)
 
